41e5770d0e614355ffd52c175fa4ff047944c9aa
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-01-21 13:42:14 -0600

    have a working calculation.

--- range_calculation_code.py
+++ range_calculation_code.py
@@ -1,1 +1,17 @@
-print('hello, world.')
+"""
+Stuff.
+More Stuff
+Spam
+Green eggs and ham.
+
+WE are going to do a range calculation.
+"""
+
+import math
+
+gravity_m_per_sec_squared = 9.807
+initial_velocity = 20
+angle_in_degrees = 40
+range = initial_velocity * initial_velocity\
+ * math.sin(math.radians(2 * angle_in_degrees)) / gravity_m_per_sec_squared
+print('our range', range)
